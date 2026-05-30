ac3e27bcfe905eb78a4a33b310b9dc42a6f12ac2
Vikas Chenna <work.vikaschenna@gmail.com>
2026-05-30 07:43:20 +0530

    added first react basic

---
+++ Basic_Project/src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+    <h1>Vikas chenna | </h1>
+    <p>
+      {/* Notes */}
+      <h2>How to install React</h2>
+      <h4>we can install with create-react or with bundlers ex :-  Vite, Parcel, or RSbuild.
+        with the create-react it install soo many libraries and extra stuff to avoid this and we dont want unnecessary stuff simple use any of this  Vite, Parcel, or RSbuild.
+
+         </h4>
+    </p>
+    </>
+  )
+}
+
+export default App
