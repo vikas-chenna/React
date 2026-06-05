850e40b4bac02e5c5349e0a82d1a90bd35ef8c6e
Vikas Chenna <work.vikaschenna@gmail.com>
2026-06-05 08:05:52 +0530

    React Interview Question sloved

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -6,9 +6,35 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  //assignment
+
+  // function addValue(){
+  //   if(count < 20) setCount(count + 1)
+  //   else return;
+  // }
+  // function removeValue(){
+  //   if(count > 0 ) setCount(count - 1)
+  //   else return;
+  // }
+
+  //problem i have written multiple setcount but because of fiber it only send update once
+  // to overcome this problem we use callback fun in that what callback do it will send one by one, one fun executed then it send another not like above one
+  // function addValue(){
+  //   setCount(count + 1)
+  //   setCount(count + 1)
+  //   setCount(count + 1)
+  //   setCount(count + 1)
+  //   setCount(count + 1)
+  //   setCount(count + 1)
+  // }
+
+  //here we are passing callback fun to executed multiple times
   function addValue(){
-    if(count < 20) setCount(count + 1)
-    else return;
+    setCount(prevCounter => prevCounter + 1)
+    setCount(prevCounter => prevCounter + 1)
+    setCount(prevCounter => prevCounter + 1)
+    setCount(prevCounter => prevCounter + 1)
+    setCount(prevCounter => prevCounter + 1)
   }
   function removeValue(){
     if(count > 0 ) setCount(count - 1)
