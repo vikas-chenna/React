175359fcca21578dbb7276e6bc0de4e8495cc60f
Vikas Chenna <work.vikaschenna@gmail.com>
2026-06-17 00:29:10 +0530

    Added Project With Context API & LocalStorage

---
+++ 05_TodoContAPILocal/src/Contexts/TodoContext.js
@@ -0,0 +1,28 @@
+import { useContext,createContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos : [
+        {
+            id : 1,
+            todo : "Todo Msg",
+            completed : false
+        }
+    ],
+
+    addTodo : (todo) => {},
+    updateTodo : (id,todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {}
+})
+
+
+export const TodoProvider = TodoContext.Provider;
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+
+
